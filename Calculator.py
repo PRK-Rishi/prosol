123b74b44d9ddb6e8f21233d293ab06af855664b
PRK-Rishi <itsrishi2004@gmail.com>
2024-07-02 10:45:09 +0530

    Calculator Building

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a Number:"))
+num2 = int(input("Enter another Number:"))
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quot = num1 / num2
+print("The sum is:",sum)
+print("The difference is:",difference)
+print("The product is:",product)
+print("The quotient is:",quot)
